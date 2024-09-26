61c259dee72ea92e821337d3e895a043a8464c7e
jongwook <project5587@google.com>
2024-09-26 15:28:44 +0900

    Update BBoom

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <Contents>
       <Content name="디시인사이드" community="dcinside"/>
-      <Content name="뽐뿌" community="BB"/>
+      <Content name="뽐" community="bb"/>
       <Content name="유튜브" community="youtube"/>
       <Content name="개드립넷" community="dog"/>
       <Content name="네이트판" community="nate"/>
